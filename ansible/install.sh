7ed84719772f5a41d7afc4f18b0c69f3649ef506
Tyler <>
2017-02-01 21:50:47 -0500

    initial

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,6 +2,8 @@
 
 yum install -y epel-release python2-pip python-devel
 
+pip install --upgrade pip
+pip install --upgrade setuptools
 pip install ansible pexpect
 
 setsebool -P httpd_can_network_connect 1
