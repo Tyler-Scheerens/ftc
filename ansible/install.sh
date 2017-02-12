70be1c1822ee0b44ed1f7e3bf9d2431675ddbc41
Tyler <>
2017-02-12 14:33:42 -0500

    initial

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y epel-release python2-pip python-devel
+yum install -y epel-release yum-priorities python2-pip python-devel
 
 pip install --upgrade pip
 pip install --upgrade setuptools
@@ -8,3 +8,16 @@ pip install ansible pexpect
 
 setsebool -P httpd_can_network_connect 1
 
+#vim /etc/yum.repos.d/CentOS-Base.repo
+#  Add the following to sections base and update
+#    exclude=postfix-*
+#    priority=1 
+#  Modify the following in section centosplus
+#    priority=2
+#    enabled=1
+
+#yum erase postfix
+#yum install postfix
+#postconf -c /etc/postfix -m | grep sql
+#  mysql
+#  pgsql
